b41c9a27ed7791c3416c637edad5e9029fc59d7b
Stanisław Horna <stanislawhorna@outlook.com>
2024-10-14 19:01:04 +0200

    add custom traces

--- src/Router/User.py
+++ src/Router/User.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter
+from opentelemetry import trace
 import src.DB as DB
 
 
 user_router = APIRouter(prefix="/user")
-
+tracer = trace.get_tracer(__name__)
 
 @user_router.get("")
 def get_user(id: int = None) -> list[DB.User]:
     """
     Returns list of users
     """
-    with DB.Session() as s:
-        return (
-            s.query(DB.User).filter(DB.User.id == id if id is not None else True).all()
-        )
+    with tracer.start_as_current_span("DB_query-select_users"):
+        with DB.Session() as s:
+            return (
+                s.query(DB.User).filter(DB.User.id == id if id is not None else True).all()
+            )
 
 
 @user_router.post("")
@@ -21,8 +23,9 @@ def post_user(user_data: DB.User):
     """
     Adds new user entry
     """
-    with DB.Session() as s:
-        s.add(user_data)
-        s.commit()
-        s.refresh(user_data)
-        return user_data
+    with tracer.start_as_current_span("DB_query-insert_user"):
+        with DB.Session() as s:
+            s.add(user_data)
+            s.commit()
+            s.refresh(user_data)
+            return user_data
